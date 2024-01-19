78073caf6ff762e496bdcc4a905fdbba0d390d8d
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-01-19 19:33:39 +0600

    added banner

---
+++ src/Components/ui/Banner/Images.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+// images.js
+
+import image1 from '../../../assets/ghfxdf.png';
+import image2 from '../../../assets/ghfgh.png';
+import image3 from '../../../assets/ghfdfg.png';
+import image4 from '../../../assets/ghf.png';
+
+
+
+
+
+
+const images = [image1, image2, image3, image4];
+
+export default images;
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -19,65 +19,67 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="bg-base-100 max-w-7xl mx-auto px-0 md:px-10 lg:px-0">
-            {/* First position */}
-            <div className="navbar">
-                <div className="navbar-start md:mr-10">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <div className="bg-white w-full">
+            <div className="bg-base-100 mx-auto px-0 md:px-10 lg:px-7">
+                {/* First position */}
+                <div className="navbar">
+                    <div className="navbar-start md:mr-10">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            {/* Mobile menu */}
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                {links}
+                            </ul>
                         </div>
-                        {/* Mobile menu */}
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <div className="flex items-center gap-2">
+                            <Image
+                                src="https://i.ibb.co/PxBs9dH/Boi-Binimoy-Transparent-Big.png" alt="card" priority width={500} height={500} style={{
+                                    width: '50px',
+                                    height: '100%',
+                                }} />
+                            <p className="text-base md:text-lg lg:text-xl font-bold">Boi Binimoy</p>
+                        </div>
+                    </div>
+                    <div className="navbar-center hidden md:flex">
+                        <input type="text" placeholder="Search products..." className="bg-base-300 text-black rounded-l-full rounded-r-full pl-4 py-2 w-[200px] md:w-[250px] lg:w-[400px]" />
+                        <button className="bg-[#f65d4e] rounded-full relative right-8"><IoSearch className="text-white m-3 w-8"></IoSearch></button>
+                    </div>
+                    <div className="navbar-end flex gap-3 md:gap-6">
+                        <button><FaRegUser className="text-lg"></FaRegUser></button>
+                        <button><FaRegHeart className="text-lg"></FaRegHeart></button>
+                        <button><BsBasket3 className="text-lg"></BsBasket3></button>
+                    </div>
+                </div>
+                <hr />
+                {/* Second Position*/}
+                <div className="navbar bg-base-100">
+                    <div className="navbar-start">
+                        {/* Category */}
+                        <div className="flex items-center gap-2 bg-[#f65d4e] text-white p-2 w-[200px] rounded-l-full rounded-r-full">
+                            <p><CgMenuGridO className="text-2xl"></CgMenuGridO></p>
+                            <p className="text-sm">Category</p>
+                            <p className="pl-10"><IoIosArrowDown></IoIosArrowDown></p>
+                        </div>
+                        {/* Search */}
+                        <div className="flex ml-4 md:hidden">
+                            <input type="text" placeholder="Search products..." className="bg-base-300 text-black rounded-l-full rounded-r-full pl-4 py-2 w-[200px]" />
+                            <button className="bg-[#f65d4e] rounded-full relative right-8"><IoSearch className="text-white m-3 w-8"></IoSearch></button>
+                        </div>
+                    </div>
+                    <div className="navbar-center hidden md:flex">
+                        <ul className="menu menu-horizontal px-1">
                             {links}
                         </ul>
                     </div>
-                    <div className="flex items-center gap-2">
-                        <Image
-                            src="https://i.ibb.co/PxBs9dH/Boi-Binimoy-Transparent-Big.png" alt="card" priority width={500} height={500} style={{
-                                width: '50px',
-                                height: '100%',
-                            }} />
-                        <p className="text-base md:text-lg lg:text-xl font-bold">Boi Binimoy</p>
-                    </div>
-                </div>
-                <div className="navbar-center hidden md:flex">
-                    <input type="text" placeholder="Search products..." className="bg-base-300 text-black rounded-l-full rounded-r-full pl-4 py-2 w-[200px] md:w-[250px] lg:w-[400px]" />
-                    <button className="bg-[#f65d4e] rounded-full relative right-8"><IoSearch className="text-white m-3 w-8"></IoSearch></button>
-                </div>
-                <div className="navbar-end flex gap-3 md:gap-6">
-                    <button><FaRegUser className="text-lg"></FaRegUser></button>
-                    <button><FaRegHeart className="text-lg"></FaRegHeart></button>
-                    <button><BsBasket3 className="text-lg"></BsBasket3></button>
-                </div>
-            </div>
-            <hr />
-            {/* Second Position*/}
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    {/* Category */}
-                    <div className="flex items-center gap-2 bg-[#f65d4e] text-white p-2 w-[200px] rounded-l-full rounded-r-full">
-                        <p><CgMenuGridO className="text-2xl"></CgMenuGridO></p>
-                        <p className="text-sm">Category</p>
-                        <p className="pl-10"><IoIosArrowDown></IoIosArrowDown></p>
-                    </div>
-                    {/* Search */}
-                    <div className="flex ml-4 md:hidden">
-                        <input type="text" placeholder="Search products..." className="bg-base-300 text-black rounded-l-full rounded-r-full pl-4 py-2 w-[200px]" />
-                        <button className="bg-[#f65d4e] rounded-full relative right-8"><IoSearch className="text-white m-3 w-8"></IoSearch></button>
-                    </div>
-                </div>
-                <div className="navbar-center hidden md:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {links}
-                    </ul>
-                </div>
-                <div className="navbar-end hidden md:flex">
-                    <div className="flex items-center gap-2">
-                        <p className="bg-gray-200 rounded-full p-2"><FiPhoneCall></FiPhoneCall></p>
-                        <div>
-                            <p className="text-[#f65d4e] text-md font-bold">+84-1800-4635</p>
-                            <p className="text-xs text-gray-400">24/7 Support Center</p>
+                    <div className="navbar-end hidden md:flex">
+                        <div className="flex items-center gap-2">
+                            <p className="bg-gray-200 rounded-full p-2"><FiPhoneCall></FiPhoneCall></p>
+                            <div>
+                                <p className="text-[#f65d4e] text-md font-bold">+84-1800-4635</p>
+                                <p className="text-xs text-gray-400">24/7 Support Center</p>
+                            </div>
                         </div>
                     </div>
                 </div>
