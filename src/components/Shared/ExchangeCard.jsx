8312ceaf368ec2547978a4f9d0db8c038a2327af
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-02-03 16:39:02 +0600

    added card styles

--- src/components/Shared/ExchangeCard.jsx
+++ src/components/Shared/ExchangeCard.jsx
@@ -2,16 +2,16 @@
 
 import "./Card.css";
 import { FaCartPlus, FaExchangeAlt } from "react-icons/fa";
+import Image from 'next/image';
 
 export default function ExchangeCard({ item }) {
   // console.log(item);
   return (
     <div className="l-container">
       <div className="b-game-card">
-        <div
-          className="b-game-card__cover"
-          style={{ backgroundImage: `url(${item.cover_image})` }}
+      <div className="b-game-card__cover book-cover-effect"
         >
+          <Image src={item.cover_image} alt="alt" width={1000} height={1000} />
           <div className="grid grid-cols-1 items-end justify-end gap-2 card__action">
             <button className=" text-white text-center text-xl border border-gray-600 border-opacity-30 backdrop-blur-md p-3 bg-black/30 rounded-full">
               <FaExchangeAlt />
@@ -26,8 +26,10 @@ export default function ExchangeCard({ item }) {
         <h2 className="text-lg font-bold text-[#016961] text-center">
           {item.title}
         </h2>
-        <p className="text-sm text-[#626980] text-center">{item.writer}</p>
+        <p className="text-sm text-[#626980] text-center"> <span>-</span> {item.writer}</p>
       </div>
     </div>
+
+
   );
 }
