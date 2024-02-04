378591c39b42ebf2f90824c1c1db5cc9a193b32f
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-02-04 16:24:27 +0600

    updated exchange book section

--- src/components/Shared/BookCard.jsx
+++ src/components/Shared/BookCard.jsx
@@ -7,9 +7,9 @@ import { FaCartPlus, FaExchangeAlt } from "react-icons/fa";
 // bg-[#f2fdf9]
 // text-[#2f8880]
 
-export default function BookCard({ item }) {
+export default function ExchangeCard({ item }) {
   return (
-    <div className="l-container md:px-0.5 ">
+    <div className="l-container md:p-1 ">
       <div className="b-game-card ">
         <div
           className="b-game-card__cover book-cover-effect"
@@ -23,9 +23,9 @@ export default function BookCard({ item }) {
               <FaCartPlus />
             </button>
           </div>
-          {/* <span className="price-tag">
+          <span className="price-tag">
             <span className="text-lg">$29.99</span>
-          </span> */}
+          </span>
         </div>
       </div>
 
@@ -36,10 +36,27 @@ export default function BookCard({ item }) {
           </h2>
           <p className="text-[13px] text-[#626980] italic line-clamp-1"> <span>-</span> {item.writer}</p>
         </div>
+
+        <div className='flex items-center truncate mt-1 text-[#62807b] text-sm'>
+          <div className='flex gap-[1px] -mt-[2px] mr-1.5'>
+            {Array.from({ length: Math.min(Math.floor(3.5), 5) }, (_, index) => (
+              <span key={index} className="text-yellow-400"><BsStarFill /></span>
+            ))}
+            {3.5 % 1 !== 0 && (
+              <span className="text-yellow-400"><BsStarHalf /> </span>
+            )}
+            {Array.from({ length: Math.max(5 - Math.ceil(3.5), 0) }, (_, index) => (
+              <span key={index} className="text-gray-400"><BsStar /></span>
+            ))}
+          </div>
+          <p>{Math.min(3.5, 5)} {Math.min(3.5, 5) > 1 ? ("Ratings") : ("Rating")}</p>
+        </div>
+
         <hr className="hr-card" />
         <div className="mt-2.5">
           <p className="text-sm text-[#62807b] line-clamp-3"> {item.description}</p>
         </div>
+
       </div>
     </div>
   );
