bf65e44c6333459d3cbbadd26730cf8ef0ab8c02
parvejme24 <mdparvejme24@gmail.com>
2024-02-06 10:35:20 +0600

    change a comment

--- src/components/UserDashboard/UserNavLeft.jsx
+++ src/components/UserDashboard/UserNavLeft.jsx
@@ -8,8 +8,6 @@ const UserNavLeft = () => {
   const { user } = useContext(AuthContext);
   const [isAdmin] = useAdmin();
 
-  // dashboard 
-
   const [componentsMounted, setComponentMounted] = useState(false);
 
   useEffect(() => {
