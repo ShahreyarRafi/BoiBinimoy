910c07a7d1ccbdbbfc7677611daddc55ba9fb2c9
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-02-08 19:50:03 +0600

    simplified couter

--- src/components/Home/Stats/Stats.jsx
+++ src/components/Home/Stats/Stats.jsx
@@ -1,84 +1,43 @@
 "use client"
 
-
-import React, { useRef, useEffect, useState } from "react";
+import React from "react";
 import CountUp from "react-countup";
 
 const Stats = () => {
-  const [isVisible, setIsVisible] = useState(false);
-  const statsRef = useRef(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsVisible(true);
-            observer.unobserve(entry.target);
-          }
-        });
-      });
-  
-      observer.observe(statsRef.current);
-  
-      // Cleanup observer on unmount
-      return () => {
-        observer.disconnect();
-      };
-    }
-  }, []);
-  
-
   return (
-    <div ref={statsRef} className="my-10 px-5">
+    <div className="my-10 px-5">
       <div className="bg-[#016961] text-white rounded-lg md:rounded-full px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-10">
         <div className="grid grid-cols-2 row-gap-8 md:grid-cols-4">
           <div className="text-center md:border-r">
-            {isVisible && (
-              <CountUp start={0} end={764} duration={2.5} separator="," suffix="K">
-                {({ countUpRef }) => (
-                  <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl" ref={countUpRef} />
-                )}
-              </CountUp>
-            )}
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl">
+              <CountUp end={144000} duration={2} />
+            </h6>
             <p className="text-sm font-medium tracking-widest text-gray-100 uppercase lg:text-base">
               Exchange
             </p>
           </div>
           <div className="text-center md:border-r">
-            {isVisible && (
-              <CountUp start={0} end={388} duration={2.5} separator="," suffix="K">
-                {({ countUpRef }) => (
-                  <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl" ref={countUpRef} />
-                )}
-              </CountUp>
-            )}
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl">
+              <CountUp end={12900} duration={2} />
+            </h6>
             <p className="text-sm font-medium tracking-widest text-gray-100 uppercase lg:text-base">
               Sale
             </p>
           </div>
           <div className="text-center md:border-r">
-            {isVisible && (
-              <CountUp start={0} end={480} duration={2.5} separator="," suffix="K">
-                {({ countUpRef }) => (
-                  <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl" ref={countUpRef} />
-                )}
-              </CountUp>
-            )}
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl">
+              <CountUp end={48300} duration={2} />
+            </h6>
             <p className="text-sm font-medium tracking-widest text-gray-100 uppercase lg:text-base">
               Users
             </p>
           </div>
           <div className="text-center">
-            {isVisible && (
-              <CountUp start={0} end={45} duration={2.5} separator="," suffix="K">
-                {({ countUpRef }) => (
-                  <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl" ref={countUpRef} />
-                )}
-              </CountUp>
-            )}
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl">
+              <CountUp end={24500} duration={2} />
+            </h6>
             <p className="text-sm font-medium tracking-widest text-gray-100 uppercase lg:text-base">
-              Writers
+              Writer
             </p>
           </div>
         </div>
