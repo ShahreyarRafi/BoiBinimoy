b60d2a61d62d15422ce7dbf5e458271b17b9a503
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-02-09 19:42:16 +0600

    fix dashboar sidebar shoing error when routes chnage

--- src/components/UserDashboard/UserNavLeft.jsx
+++ src/components/UserDashboard/UserNavLeft.jsx
@@ -48,19 +48,23 @@ const UserNavLeft = () => {
     const allSideMenu = document.querySelectorAll(
       "#sidebar .side-menu.top li a"
     );
-
+    
     allSideMenu.forEach((item) => {
       const li = item.parentElement;
-
-      item.addEventListener("click", function () {
-        allSideMenu.forEach((i) => {
-          i.parentElement.classList.remove("active");
+      if (li) { // Check if li exists
+        item.addEventListener("click", function () {
+          allSideMenu.forEach((i) => {
+            const parentLi = i.parentElement;
+            if (parentLi) { // Check if parentLi exists
+              parentLi.classList.remove("active");
+            }
+          });
+          li.classList.add("active");
         });
-        li.classList.add("active");
-      });
-      // console.log(li);
+      }
     });
-
+    
+    
     // TOGGLE SIDEBAR
     const menuBar = document.querySelector("#content nav .bx.bx-menu");
     const sidebar = document.getElementById("sidebar");
