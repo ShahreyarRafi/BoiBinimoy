765d12788ebd88a95b1d3996e77541f66c168e15
parvejme24 <mdparvejme24@gmail.com>
2024-02-17 23:31:31 +0600

    added search box in publisher page

--- src/app/publisher/page.js
+++ src/app/publisher/page.js
@@ -1,5 +1,7 @@
 "use client";
+import Header from "@/components/Home/Header/Header";
 import PublisherComponent from "@/components/Publisher/Publisher";
+import PublisherBanner from "@/components/Publisher/PublisherBanner";
 import Footer from "@/components/Shared/Footer/Footer";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 import React from "react";
@@ -8,6 +10,7 @@ export default function PublisherPage() {
   return (
     <div>
       <Navbar />
+      <PublisherBanner />
       <PublisherComponent />
       <Footer />
     </div>
