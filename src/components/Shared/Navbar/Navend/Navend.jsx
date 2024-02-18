6c6396f231c5e9f50e8ff72d5dd1ed912591a739
Minhajul-alam <alam.minhajul@outlook.com>
2024-02-18 16:01:47 +0600

    Navend error solved

--- src/components/Shared/Navbar/Navend/Navend.jsx
+++ src/components/Shared/Navbar/Navend/Navend.jsx
@@ -7,7 +7,7 @@ import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "@/providers/AuthProvider";
 import Image from "next/image";
 import Link from "next/link";
-import axios from 'axios';
+import axios from "axios";
 import PageLoading from "../../loadingPageBook/PageLoading";
 import useOneUser from "@/Hooks/Users/useOneUser";
 
@@ -26,9 +26,11 @@ const Navend = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`https://boi-binimoy-server.vercel.app/api/v1/my-carts/${user?.email}`);
+        const response = await fetch(
+          `https://boi-binimoy-server.vercel.app/api/v1/my-carts/${user?.email}`
+        );
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const result = await response.json();
         setCart(result);
@@ -42,92 +44,136 @@ const Navend = () => {
     fetchData();
   }, [user?.email, totalPrice]);
 
-  cart?.books?.map(book => totalPrice = parseFloat(totalPrice) + parseFloat(book.price))
+  cart?.books?.map(
+    (book) => (totalPrice = parseFloat(totalPrice) + parseFloat(book.price))
+  );
 
   return (
     <div className="flex items-center gap-2">
-      <MdFavoriteBorder />
+      <div className="text-2xl">
+        <MdFavoriteBorder />
+      </div>
 
       {/* Drawer cart */}
       <div className="drawer drawer-end">
         <input id="cart-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* Page content here */}
-          <label htmlFor="cart-drawer" className="drawer-button"><MdOutlineShoppingCart /></label>
+          <label htmlFor="cart-drawer" className="drawer-button">
+            <MdOutlineShoppingCart />
+          </label>
         </div>
         <div className="drawer-side z-[2]">
-          <label htmlFor="cart-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <label
+            htmlFor="cart-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
           <ul className="menu p-4 w-1/3 min-h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
-            {
-              cart?.books?.map(book => <li key={book?._id}><div className='flex items-center justify-between rounded-lg p-2'>
-                <div className="flex gap-5 items-center">
-                  <Image
-                    src={book?.cover_image}
-                    width={70}
-                    height={100}
-                    alt="book"
-                    priority
-                    style={{ width: "70px", height: "100px" }}
-                    className="rounded-md"
-                  />
-                  <div>
-                    <h2 className="font-bold text-lg">{book?.title}</h2>
-                    <h2 className="text-orange-700 font-bold text-lg">{book?.price} BDT</h2>
+            {cart?.books?.map((book) => (
+              <li key={book?._id}>
+                <div className="flex items-center justify-between rounded-lg p-2">
+                  <div className="flex gap-5 items-center">
+                    <Image
+                      src={book?.cover_image}
+                      width={70}
+                      height={100}
+                      alt="book"
+                      priority
+                      style={{ width: "70px", height: "100px" }}
+                      className="rounded-md"
+                    />
+                    <div>
+                      <h2 className="font-bold text-lg">{book?.title}</h2>
+                      <h2 className="text-orange-700 font-bold text-lg">
+                        {book?.price} BDT
+                      </h2>
+                    </div>
                   </div>
+                  <button
+                    onClick={() => handleDelete(book?._id)}
+                    className="mt-5 button-color px-4 py-2 rounded-full text-sm md:text-base text-white flex items-center gap-1"
+                  >
+                    <RxCross2></RxCross2>
+                  </button>
                 </div>
-                <button onClick={() => handleDelete(book?._id)} className="mt-5 button-color px-4 py-2 rounded-full text-sm md:text-base text-white flex items-center gap-1"><RxCross2></RxCross2></button>
-              </div>
                 <hr />
-              </li>)
-            }
+              </li>
+            ))}
             <li className="mx-auto">
               <h3 className="text-xl font-bold">Total: {totalPrice}</h3>
             </li>
-            <li><hr /></li>
-            <li className="mx-auto"><div className="flex gap-5 items-center">
-              <Link href="/cart" className="button-color px-4 py-2 rounded-full text-sm md:text-base text-white flex items-center gap-1">View cart</Link>
-              <button className="button-color px-4 py-2 rounded-full text-sm md:text-base text-white flex items-center gap-1">Checkout</button>
-            </div></li>
+            <li>
+              <hr />
+            </li>
+            <li className="mx-auto">
+              <div className="flex gap-5 items-center">
+                <Link
+                  href="/cart"
+                  className="button-color px-4 py-2 rounded-full text-sm md:text-base text-white flex items-center gap-1"
+                >
+                  View cart
+                </Link>
+                <button className="button-color px-4 py-2 rounded-full text-sm md:text-base text-white flex items-center gap-1">
+                  Checkout
+                </button>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
 
-
-
       <div className="navbar-end">
-        {user ? <div>
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-
-                <Image
-                  src={currentUser.image}
-                  alt="user"
-                  priority
-                  width={300}
-                  height={300}
-                />
-              </div>
-            </div>
-            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-              <li><Link href="/dashboard" className="px-4 py-2 hover:bg-base-300 rounded-lg text-black">
-                Dashboard
-              </Link></li>
-              <li> <button
-                onClick={logOut}
-                className="cursor-pointer text-red-500 px-4 py-2 hover:bg-base-300 rounded-lg"
+        {user ? (
+          <div>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
               >
-                Logout
-              </button></li>
-            </ul>
+                <div className="w-10 rounded-full">
+                  <Image
+                    src={currentUser.image}
+                    alt="user"
+                    priority
+                    width={300}
+                    height={300}
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <Link
+                    href="/dashboard"
+                    className="px-4 py-2 hover:bg-base-300 rounded-lg text-black"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <button
+                    onClick={logOut}
+                    className="cursor-pointer text-red-500 px-4 py-2 hover:bg-base-300 rounded-lg"
+                  >
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div> : <Link href="/joinUs" className="text-lg">
-          <LuUser2 />
-        </Link>
-        }
-      </div >
-    </div >
+        ) : (
+          <Link href="/joinUs" className="text-lg">
+            <LuUser2 />
+          </Link>
+        )}
+      </div>
+    </div>
   );
 };
 
