38f1b587f414d6f90aa18251b86a25bd3f554a14
Minhajul-alam <alam.minhajul@outlook.com>
2024-02-25 09:17:52 +0600

    Update blog page UI

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -18,16 +18,19 @@ const Blog = () => {
     },
   });
 
-  const main = blogs[0];
-  const sides = [blogs[1], blogs[2], blogs[3], blogs[4]];
-  const latests = [blogs[5], blogs[6], blogs[7]];
+  // const main = blogs[0];
+  // const sides = [blogs[1], blogs[2], blogs[3], blogs[4]];
+  // const latests = [blogs[5], blogs[6], blogs[7]];
+  const mains = [blogs[0], blogs[4], blogs[2], blogs[3]];
+  const sides = [blogs[5], blogs[1], blogs[6]];
 
   return (
     <div className="max-w-6xl mx-auto mt-8 mb-36">
       <main className="mt-12">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
+        {/* <div className="grid grid-cols-1 lg:grid-cols-2 gap-3"> */}
+        <div className="flex flex-col lg:flex-row gap-3">
           {/* main blog */}
-          <div className="mb-4 lg:mb-0  p-4 lg:p-0 w-full md:w-4/7 relative rounded block">
+          {/* <div className="mb-4 lg:mb-0  p-4 lg:p-0 w-full md:w-4/7 relative rounded block">
             <Image
               src={main?.cover_image}
               priority
@@ -49,10 +52,47 @@ const Blog = () => {
             >
               Read more
             </Link>
+          </div> */}
+
+          <div className="w-full">
+            {mains?.map((main) => (
+              <div key={main?._id} className="mb-10">
+                <Image
+                  src={main?.cover_image}
+                  priority
+                  width={500}
+                  height={500}
+                  alt="Main blog"
+                  className="w-full h-[300px]"
+                />
+                <span className="text-[#016961] text-sm hidden md:block mt-4">
+                  Technology
+                </span>
+                <h1 className="text-black text-4xl font-bold mt-2 mb-2 leading-tight">
+                  {main?.title}
+                </h1>
+                <p className="text-gray-700 mt-2">
+                  {main?.body[0].slice(0, 100) + "..."}
+                  <Link href={`/blogs/${main?._id}`} className="text-blue-400">
+                    Read more
+                  </Link>
+                </p>
+                {/* <p className="text-black mb-4">{main?.body}</p>
+                <Link
+                  href={`/blogs/${main?._id}`}
+                  className="inline-block px-6 py-3 mt-2 rounded-md bg-[#016961] hover:bg-[#1e4743] text-white"
+                >
+                  Read more
+                </Link> */}
+              </div>
+            ))}
           </div>
 
           {/* side blog */}
           <div className="w-full md:w-4/7 px-4">
+            <h1 className="text-3xl font-semibold text-[#016961] pb-5">
+              Populer
+            </h1>
             {sides?.map((blog) => (
               <BlogSideCard key={blog?._id} item={blog}></BlogSideCard>
             ))}
@@ -60,7 +100,7 @@ const Blog = () => {
         </div>
 
         {/* Latest blog */}
-        <div className="flex mt-16 mb-4 px-4 lg:px-0 items-center justify-between">
+        {/* <div className="flex mt-16 mb-4 px-4 lg:px-0 items-center justify-between">
           <h2 className="font-bold text-xl md:text-3xl">Latest blog</h2>
           <Link
             href="/allBlog"
@@ -68,13 +108,13 @@ const Blog = () => {
           >
             View all
           </Link>
-        </div>
+        </div> */}
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-5">
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 px-5">
           {latests?.map((blog) => (
             <BlogLatestCard key={blog?._id} item={blog}></BlogLatestCard>
           ))}
-        </div>
+        </div> */}
 
         {/* news later subscription */}
         {/* <div className="rounded flex md:shadow my-12 bg-[#016961] ">
