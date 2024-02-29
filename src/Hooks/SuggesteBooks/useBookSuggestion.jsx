219364dd2b52dfc6afe37247f9bf7f2a6e499399
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-03-01 03:10:43 +0600

    updated hook for book suggestion

--- src/Hooks/SuggesteBooks/useBookSuggestion.jsx
+++ src/Hooks/SuggesteBooks/useBookSuggestion.jsx
@@ -1,36 +1,42 @@
 import { useEffect, useState } from 'react';
 import useOneUser from '../Users/useOneUser';
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from '../Axios/useAxiosPublic';
 
 const useBookSuggestion = (CurrentlyViewing) => {
     const { interest } = useOneUser();
+    const axiosPublic = useAxiosPublic();
+
+    const { data: bookDetails = [], isLoading } = useQuery({
+        queryKey: ["bookDetails", interest?.book],
+        queryFn: async () => {
+            const bookDetailsPromises = interest?.book?.map(async (_id) => {
+                try {
+                    const response = await axiosPublic.get(`/api/v1/buy-books/${_id}`);
+                    if (response.status !== 200) {
+                        throw new Error('Failed to fetch book details');
+                    }
+                    return response.data;
+                } catch (error) {
+                    console.error(error);
+                    return null;
+                }
+            });
+            const books = await Promise.all(bookDetailsPromises);
+            return books.filter(book => book !== null);
+        },
+    });
+
     const [bookSuggestion, setBookSuggestion] = useState([]);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const fetchBookDetails = async () => {
-            try {
-                const bookDetailsPromises = interest?.book?.map(async (bookId) => {
-                    const response = await fetch(`https://boi-binimoy-server.vercel.app/api/v1/buy-books/${bookId}`);
-                    if (!response.ok) {
-                        throw new Error('Failed to fetch book details');
-                    }
-                    const bookData = await response.json();
-                    return bookData;
-                });
-                const books = await Promise.all(bookDetailsPromises);
-
-                const filteredBooks = books.filter(book => book._id !== CurrentlyViewing);
-
-                setBookSuggestion(filteredBooks);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchBookDetails();
-    }, [interest, CurrentlyViewing]);
+        if (!isLoading) {
+            const filteredBooks = bookDetails.filter(book => book._id !== CurrentlyViewing);
+            setBookSuggestion(filteredBooks);
+            setLoading(false);
+        }
+    }, [bookDetails, CurrentlyViewing, isLoading]);
 
     return { bookSuggestion, loading };
 };
