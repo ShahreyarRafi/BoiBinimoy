e2969430ccd5447e3095502a1f9e6eb35b463c7d
ShahreyarRafi <shahreyar.rafi@gmail.com>
2024-03-04 15:21:53 +0600

    change image tag in banner for testing

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -101,7 +101,7 @@ export default function BannerNew() {
                 <div className="list">
                     {Array.isArray(bannerData) && bannerData.map((item, index) => (
                         <div className="item" key={index}>
-                            <Image src={item?.cover_image} height={4100} width={2310} alt="alt" />
+                            <img src={item?.cover_image} alt="alt" />
                             <div className="content">
                                 <div className="author">{item?.author}</div>
                                 <div className={`${sourceSerif.className} title font-outline`}>{item?.title}</div>
