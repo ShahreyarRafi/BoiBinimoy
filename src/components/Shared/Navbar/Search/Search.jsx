7a51ce4a0e1e8ef54983f35e64792fc81d537a9b
Minhajul-alam <alam.minhajul@outlook.com>
2024-03-07 10:28:18 +0600

    Added search suggeation UI

--- src/components/Shared/Navbar/Search/Search.jsx
+++ src/components/Shared/Navbar/Search/Search.jsx
@@ -1,5 +1,6 @@
 import { IoSearchSharp } from "react-icons/io5";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,6 +24,27 @@ const Search = () => {
           <IoSearchSharp />
         </button>
       </form>
+
+      {/* <div className="max-w-3xl mx-auto flex items-center gap-3 hover:bg-gray-300 py-2 px-3 rounded-sm border-b">
+        <div>
+          <Image
+            width={30}
+            height={30}
+            alt=""
+            src="https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Jibone_Sofolotar_Jonno_Moner_Shokti-_Sabit_Rayhan-40864-298229.jpg"
+          />
+        </div>
+        <div className="w-full">
+          <h1 className="font-semibold">Moner Sokti</h1>
+          <p className="text-xs text-gray-400">Sabit Raihan</p>
+        </div>
+
+        <div className="flex items-center font-light gap-2 min-w-fit">
+          <p className="text-sm text-green-600">Insok</p>
+          <p className="text-sm text-red-600">(20% off)</p>
+          <h3 className="font-semibold">503$</h3>
+        </div>
+      </div> */}
     </>
   );
 };
